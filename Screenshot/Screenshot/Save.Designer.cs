6576b3ad0d346cd97761d8e85b90f17bf99bd84c
thedava <tehdava@googlemail.com>
2016-03-02 21:30:48 +0100

    Initial commit

---
+++ Screenshot/Screenshot/Save.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Screenshot
+{
+    partial class Save
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSaving = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblSaving
+            // 
+            this.lblSaving.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblSaving.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaving.Location = new System.Drawing.Point(0, 0);
+            this.lblSaving.Name = "lblSaving";
+            this.lblSaving.Size = new System.Drawing.Size(284, 86);
+            this.lblSaving.TabIndex = 0;
+            this.lblSaving.Text = "Saving...";
+            this.lblSaving.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Save
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(284, 86);
+            this.Controls.Add(this.lblSaving);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Save";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Screenshot";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Save_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSaving;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Screenshot/Screenshot/Save.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Screenshot
+{
+    public partial class Save : Form
+    {
+        const string Folder = "Screenshots";
+        const string FileName = "Screenshot";
+        const string DateFormat = "yyyy-MM-dd";
+        const string Extension = ".png";
+
+        private bool Done { get; set; }
+
+        public Save()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Load(object sender, System.EventArgs e)
+        {
+            if (!Clipboard.ContainsImage())
+            {
+                this.lblSaving.Text = "Aborting...";
+                MessageBox.Show("Clipboard doesn't contain an image.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Done = true;
+                return;
+            }
+
+            var folder = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + Path.DirectorySeparatorChar + Folder;
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            var image = Clipboard.GetImage();
+            try
+            {
+                image.Save(folder + Path.DirectorySeparatorChar + FileName + "_" + DateTime.Now.ToString(DateFormat) + Extension);
+                Process.Start(folder);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                this.Done = true;
+            }
+            
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (this.Done)
+            {
+                this.timer.Stop();
+                this.Close();
+            }
+        }
+    }
+}
